1cf5a5b8d891415e1f66f933e0901e0c12b6cefc
ADARSH CHOUKSEY <choukseyaryan49@gmail.com>
2025-07-26 18:51:40 +0530

    modified app and server

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,110 @@
-import express from 'express';
-import morgan from 'morgan';
-import connectDB from './db/db.js';
-import userRoutes from './routes/user.routes.js'
-import projectRoutes from './routes/project.routes.js'
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-const app=express();
-connectDB();
+import 'dotenv/config';
+import http from 'http';
+import app from './app.js';
+import { Server } from 'socket.io';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+import projectModel from './models/project.model.js';
+import { generateResult } from './services/ai.service.js';
 
-app.use(morgan('dev'))
-app.use(cors());
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
 
-app.use(express.urlencoded({extended:true}))
 
-app.use(cookieParser());
-
-app.use('/users',userRoutes)
-app.use('/projects',projectRoutes);
-
-app.get('/',(req,res)=>{
-    res.send('Hello World')
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    }
 });
 
-export default app;
+
+io.use(async (socket, next) => {
+
+    try {
+
+        const token = socket.handshake.auth?.token || socket.handshake.headers.authorization?.split(' ')[ 1 ];
+        const projectId = socket.handshake.query.projectId;
+
+        if (!mongoose.Types.ObjectId.isValid(projectId)) {
+            return next(new Error('Invalid projectId'));
+        }
 
 
+        socket.project = await projectModel.findById(projectId);
+
+
+        if (!token) {
+            return next(new Error('Authentication error'))
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decoded) {
+            return next(new Error('Authentication error'))
+        }
+
+
+        socket.user = decoded;
+
+        next();
+
+    } catch (error) {
+        next(error)
+    }
+
+})
+
+
+io.on('connection', socket => {
+    socket.roomId = socket.project._id.toString()
+
+
+    console.log('a user connected');
+
+
+
+    socket.join(socket.roomId);
+
+    socket.on('project-message', async data => {
+
+        const message = data.message;
+
+        const aiIsPresentInMessage = message.includes('@ai');
+        socket.broadcast.to(socket.roomId).emit('project-message', data)
+
+        if (aiIsPresentInMessage) {
+
+
+            const prompt = message.replace('@ai', '');
+
+            const result = await generateResult(prompt);
+
+
+            io.to(socket.roomId).emit('project-message', {
+                message: result,
+                sender: {
+                    _id: 'ai',
+                    email: 'AI'
+                }
+            })
+
+
+            return
+        }
+
+
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        socket.leave(socket.roomId)
+    });
+});
+
+
+
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
