6413c5fd8589f36f80ef1994cb038c8ae0191d6a
ADARSH CHOUKSEY <choukseyaryan49@gmail.com>
2025-07-26 18:45:39 +0530

    added ai and modified existing services

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -1,17 +1,15 @@
 import Redis from 'ioredis';
 
+
 const redisClient = new Redis({
-    port: process.env.REDIS_PORT ,
     host: process.env.REDIS_HOST,
-    password: process.env.REDIS_PASSWORD,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD
 });
+
 
 redisClient.on('connect', () => {
-    console.log('Connected to Redis');
-});
-redisClient.on('error', (err) => {
-  console.error('Redis connection error:', err);
-});
+    console.log('Redis connected');
+})
 
 export default redisClient;
-
