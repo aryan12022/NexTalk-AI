06eb38c982cd0ae9303311192f64acc79e2219aa
ADARSH CHOUKSEY <choukseyaryan49@gmail.com>
2025-07-26 18:11:37 +0530

    modified project model

--- backend/models/project.model.js
+++ backend/models/project.model.js
@@ -1,20 +1,30 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+
 
 const projectSchema = new mongoose.Schema({
-
     name: {
         type: String,
         lowercase: true,
         required: true,
         trim: true,
-        unique: true
+        unique: [ true, 'Project name must be unique' ],
     },
-    users:[{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user" 
-    }
-]
+
+    users: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user'
+        }
+    ],
+    fileTree: {
+        type: Object,
+        default: {}
+    },
+
 })
 
+
 const Project = mongoose.model('project', projectSchema)
+
+
 export default Project;
